74dd8988b663275cae790366f67a960ff042758a
Alexis Luque <aluque@indexlabs.com.uy>
2018-05-15 17:35:12 -0300

    Update exec-spa-api.sh

--- 03-Calling-an-API/exec-spa-api.sh
+++ 03-Calling-an-API/exec-spa-api.sh
@@ -14,83 +14,79 @@ if [ "$1" =  "" ]; then
 fi
 
 # List of API/Backend
-backends=( aspnet-core1 aspnet-core2 django falcor golang hapi laravel express php python ruby rubyonrails spring-security symfony )
+backends=( aspnet-core-webapi-v1_1 aspnet-core-webapi django falcor golang hapi laravel nodejs php python ruby rails java-spring-security symfony )
 
 # Check the 'backend' arg is in backends list
 exists_backend="false"
 for ((i=0; i<${#backends[@]}; i++)); do
   if [ ${backends[${i}]} = $1 ]; then
     exists_backend="true"
-    echo ${backends[${i}]}
   fi
 done
 
 if [ "$exists_backend" = "false" ]; then
   echo "Backend must be one of the following:"
-  echo "aspnet-core1"
-  echo "aspnet-core2"
+  echo "aspnet-core-webapi-v1_1"
+  echo "aspnet-core-webapi"
   echo "django"
   echo "falcor"
   echo "golang"
   echo "hapi"
+  echo "java-spring-security"
   echo "laravel"
-  echo "express"
+  echo "nodejs"
   echo "php"
   echo "python"
   echo "ruby"
-  echo "rubyonrails"
-  echo "spring-security"
+  echo "rails"
   echo "symfony"
   exit 1
 fi
 
-# Setup the repo for the backend
-if [ $1 = aspnet-core1 ]
+# Get the repo name, organization and branch from Auth0 docs
+curl https://raw.githubusercontent.com/auth0/docs/new-download-page/articles/quickstart/backend/$1/index.yml > temp.txt
+org=$(cat temp.txt | grep org | cut -f 2 -d ':' | xargs)
+
+repo=$(cat temp.txt | grep repo | cut -f 2 -d ':' | xargs)
+
+branch=$(cat temp.txt | grep branch | cut -f 2 -d ':' | xargs)
+
+rm temp.txt
+
+if [ "$branch" = "" ]
 then
-  repo=auth0-samples/auth0-aspnetcore-webapi-samples.git#v1:Quickstart/01-Authorization
-elif [ $1 = aspnet-core2 ]
-then
-  repo=auth0-samples/auth0-aspnetcore-webapi-samples.git#master:Quickstart/01-Authorization
-elif [ $1 = django ]
-then
-  repo=auth0-samples/auth0-django-api.git#master:01-Authorization
-elif  [ $1 = falcor ]
-then
-  repo=auth0-community/auth0-falcor-sample.git#master:01-Authorization-RS256
-elif [ $1 = golang ]
-then
-  repo=auth0-samples/auth0-golang-api-samples.git#master:01-Authorization-RS256
-elif [ $1 = hapi ]
-then
-  repo=auth0-samples/auth0-hapi-api-samples.git#master:01-Authenticate-RS256
-elif [ $1 = laravel ]
-then
-  repo=auth0-samples/auth0-laravel-api-samples.git#master:01-Authorization-RS256
-elif [ $1 = express ]
-then
-  repo=auth0-samples/auth0-express-api-samples.git#master:01-Authorization-RS256
-elif [ $1 = php ]
-then
-  repo=auth0-samples/auth0-php-api-samples.git#master:01-Authenticate-RS256
-elif [ $1 = python ]
-then
-  repo=auth0-samples/auth0-python-api-samples.git#master:00-Starter-Seed
-elif [ $1 = ruby ]
-then
-  repo=auth0-samples/auth0-ruby-api-samples.git#master:01-Authorization-RS256
-elif [ $1 = rubyonrails ]
-then
-  repo=auth0-samples/auth0-rubyonrails-api-samples.git#master:01-Authentication-RS256
-elif [ $1 = spring-security ]
-then
-  repo=auth0-samples/auth0-spring-security-api-sample.git#master:01-Authorization
-elif [ $1 = symfony ]
-then
-  repo=auth0-samples/auth0-symfony-api-samples.git#master:01-Authorization-RS256
+  branch=master
 fi
 
+# Setup the directory for the backend
+if [ $1 = aspnet-core-webapi-v1_1 ] || [ $1 = aspnet-core-webapi ]
+then
+  dir=Quickstart/01-Authorization
+elif [ $1 = django ]
+then
+  dir=01-Authorization
+elif  [ $1 = falcor ] || [ $1 = golang ] || [ $1 = laravel ] || [ $1 = nodejs ] || [ $1 = ruby ] || [ $1 = symfony ]
+then
+  dir=01-Authorization-RS256
+elif [ $1 = hapi ] || [ $1 = php ]
+then
+  dir=01-Authenticate-RS256
+elif [ $1 = python ]
+then
+  dir=00-Starter-Seed
+elif [ $1 = rails ]
+then
+  dir=01-Authentication-RS256
+elif [ $1 = java-spring-security ]
+then
+  dir=01-Authorization
+fi
+
+# Github repo url
+repo_url=${org}/${repo}.git#${branch}:${dir}
+
 # Set up env variables
-export REPO=${repo}
+export REPO=${repo_url}
 # Get the value from the variable AUTH0_AUDIENCE from .env file.
 AUDIENCE=$(cat .env | grep AUTH0_AUDIENCE | cut -f 2 -d '=')
 export AUDIENCE
